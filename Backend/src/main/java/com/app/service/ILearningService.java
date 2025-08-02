e02e9bdab3a3c144bde9b3ac734954745fcda841
ganeshpatil212302 <you@example.com>
2025-08-02 21:25:06 +0530

    ILearningService.java service class done

---
+++ Backend/src/main/java/com/app/service/ILearningService.java
@@ -0,0 +1,41 @@
+package com.app.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.mail.MailException;
+
+import com.app.entities.LearningLicense;
+
+public interface ILearningService {
+
+	// for saving LL object 
+	String applyForLearning(LearningLicense ll) throws MailException, InterruptedException;
+
+	// for finding LL object associated with the user using userId
+	LearningLicense registerForLearning(LearningLicense ll, Integer userId) throws InterruptedException;
+
+	// method to find LL obj by applicationId
+	Optional<LearningLicense> findById(Integer applicantId);
+
+	// method to find LL obj by userId
+	LearningLicense findByUserId(Integer userId);
+
+	// method to find LL obj by userId
+	LearningLicense findByUserEmail(String email);
+
+	// method to delete LL obj
+	void deleteLearningLicenseById(Integer applicantId);
+
+	// method for updating LL obj
+	boolean updateLicense(LearningLicense ll);
+
+	void saveUpdatedLL(LearningLicense ll);
+
+	ArrayList<LearningLicense> getTomorrowApplicants();
+
+	ArrayList<LearningLicense> getApplicantsByDate(LocalDate date);
+
+}
